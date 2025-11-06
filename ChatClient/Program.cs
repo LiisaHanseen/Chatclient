bd3d3eace84c99b7644e748790685bf2e0644129
Lisa <lishan98@hotmail.com>
2025-11-06 18:58:18 +0100

    Add project files.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        // ----------------------------------------------------- Programmets start - Main ----------------------------------------------------------
+        static async Task Main()
+        {
+            // hantering av användarnamn, felhantering, ansluten, loop, kommandon, meddelanden, hsitorik, avslutning
+        }
+    }
+}
---
+++ ChatClient/SocketManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SocketIOClient;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatClient_Exam
+{
+    public class SocketManager
+    {
+        // server, eventnamn
+        // hantering av meddelanden, anslutning, frånkoppling, väntetid, felhantering
+    }
+}
